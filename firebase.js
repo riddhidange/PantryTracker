bf65218047c624fcba1d1da4a3ae1783fc54d966
riddhidange <riddhi.dange@gmail.com>
2024-08-05 19:59:13 -0700

    Adding project files to repo

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCI7QL76cTZFQyUWajMEvX97fWEVLP74Nw",
+  authDomain: "inventorymanagement-a3b6e.firebaseapp.com",
+  projectId: "inventorymanagement-a3b6e",
+  storageBucket: "inventorymanagement-a3b6e.appspot.com",
+  messagingSenderId: "546731898493",
+  appId: "1:546731898493:web:eb4e4a619d64a0a2a42314",
+  measurementId: "G-G3C4EXVGDS"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+export { firestore };
